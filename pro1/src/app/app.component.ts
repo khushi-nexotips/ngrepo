2572e8afacca788210d871efb0ee9c5d5c85726d
khushi-nexotips <khushi.nexotips@gmail.com>
2024-06-28 11:14:45 +0530

    add component nd modify it

--- pro1/src/app/app.component.ts
+++ pro1/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudComponent } from './component/stud/stud.component';
+import { EmpComponent } from "./component/emp/emp.component";
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, StudComponent, EmpComponent]
 })
 export class AppComponent {
   title = 'pro1';
---
+++ pro1/src/app/component/stud/stud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stud',
+  standalone: true,
+  imports: [],
+  templateUrl: './stud.component.html',
+  styleUrl: './stud.component.css'
+})
+export class StudComponent {
+
+}
