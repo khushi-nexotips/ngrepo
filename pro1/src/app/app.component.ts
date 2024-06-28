20ee234ced963de3efeb76682df660cd744cece6
khushi-nexotips <khushi.nexotips@gmail.com>
2024-06-28 15:02:31 +0530

    updates

--- pro1/src/app/app.component.ts
+++ pro1/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StudComponent } from './component/stud/stud.component';
+import { StudComponent } from "./component/stud/stud.component";
 import { EmpComponent } from "./component/emp/emp.component";
 
 @Component({
